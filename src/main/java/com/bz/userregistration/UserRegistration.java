96858079c9c3746d213939bbb5a95186bdf603a1
Adityachess <adityadone9398@gmail.com>
2021-10-23 22:47:54 +0530

    Added:User need to enter a valid last name.

--- src/main/java/com/bz/userregistration/UserRegistration.java
+++ src/main/java/com/bz/userregistration/UserRegistration.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-
 	static Scanner sc = new Scanner(System.in);
 
 	public void validFirstName() {
@@ -13,8 +12,15 @@ public class UserRegistration {
 		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
 	}
 
+	public void validLastName() {
+		System.out.println("Enter your last Name : ");
+		String lastName = sc.nextLine();
+		System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
+	}
+
 	public static void main(String[] args) {
 		UserRegistration object = new UserRegistration();
 		object.validFirstName();
+		object.validLastName();
 	}
 }
